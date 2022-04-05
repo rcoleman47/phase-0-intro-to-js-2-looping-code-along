7016baa154929b0160ccfc21ef716f540be00b06
ryanadams <rcoleman.adams@gmail.com>
2022-04-05 16:40:05 -0500

    commit tree ec3b19368f2ab19a0cb27400d823904a9970109a

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(nameArray, event){
+    let messageArray = []
+    for(let i=0; i < nameArray.length; i++){
+        let message = `Thank you, ${nameArray[i]}, for the wonderful ${event} gift!`
+        messageArray.push(message);
+    }
+    return messageArray;
+};
+
+function countDown(value){
+    let integer = value;
+    while(integer >=0){
+        console.log(integer--);
+    }
+}
